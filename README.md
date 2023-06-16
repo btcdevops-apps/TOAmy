5b9b210a7922548c81ac51246653f258c9ef988a
Oladayo Salawu btcdev01 <btcdevops.developer01@btcserv.co.uk>
2023-06-16 21:56:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tasks of Affirmation
+
+This is a wholesome todo list application. The purpose of this application is to help users stay organized in their day-to-day lives without feeling the pressure or guilt that can sometimes arise from traditional todo list applicaitons.
+
+As we build out this project, our goal will reemain on how we can look after users and provide a wholesome and helpful experience as they become and stay organized.
+
+## Stream History
---
+++ app/src/main/java/co/uk/btcserv/apps/poc/toa/MainActivity.kt
@@ -0,0 +1,43 @@
+package co.uk.btcserv.apps.poc.toa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.uk.btcserv.apps.poc.toa.ui.theme.TOATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TOATheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TOATheme {
+        Greeting("Android")
+    }
+}
